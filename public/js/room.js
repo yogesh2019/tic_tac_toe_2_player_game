c870363fe56bbfbfed51470123e6dfc60a3b2ab5
Yogesh Khichi <ykhichi@aptean.com>
2023-01-06 16:12:18 +0530

    f.on('user-con',mess.innerht='user_con') myc=X, othC=O, enableC true emit can-play f.on'can-play' myc=O othC=X, enableC true
    clicked = (id) =>{
        if(enbl)
        moves += 1;
        elementInnerHtm=myclick
        element.onclick=disable
        emit(clicked,id)
        enablC = false
        gamest[id-1] = 1
        cond()
        innerH you win
        enableC false
        setTimeout
        if(moved==9)
        setTimeout location.href
    }
    for other person socket.on(clicked)

--- public/js/room.js
+++ public/js/room.js
@@ -16,3 +16,49 @@ const copyToClip = () => {
     window.getSelection.removeAllRanges();
 }
 socket.emit("join-room", ROOM_ID);
+
+
+socket.on('user-connected', () => {
+    document.getElementById('message').innerHTML = "User Connected";
+    myClick = 'X';
+    otherClick = 'O';
+    enableClick = true,
+        socket.emit('can-play');
+})
+
+socket.on("can-play", () => {
+    myClick = 'O',
+        otherClick = 'X',
+        enableClick = true
+})
+
+const clicked = (id) => {
+    if (enableClick) {
+        moves += 1;
+        const element = document.getElementById(id);
+        element.innerHTML = myClick;
+        document.onclick = null;
+        socket.emit("clicked", id);
+        enableClick = false;
+        gameStatus[id - 1] = 1;
+        if ((gameStatus[0] == 1 && gameStatus[1] == 1 && gameStatus[2] == 1) ||
+            (gameStatus[0] == 1 && gameStatus[3] == 1 && gameStatus[6] == 1) ||
+            (gameStatus[0] == 1 && gameStatus[4] == 1 && gameStatus[8] == 1) ||
+            (gameStatus[2] == 1 && gameStatus[5] == 1 && gameStatus[8] == 1) ||
+            (gameStatus[2] == 1 && gameStatus[4] == 1 && gameStatus[6] == 1) ||
+            (gameStatus[1] == 1 && gameStatus[4] == 1 && gameStatus[7] == 1) ||
+            (gameStatus[3] == 1 && gameStatus[4] == 1 && gameStatus[5] == 1) ||
+            (gameStatus[6] == 1 && gameStatus[7] == 1 && gameStatus[8] == 1)) {
+            document.getElementById("message").innerHTML = "You win";
+            enableClick = false;
+            setTimeout(() => { location.href = '/'; }, 2000);
+        } else if (moves == 9) {
+            document.getElementById("message").innerHTML = "It's a Draw";
+            setTimeout(() => { location.href = '/'; }, 2000);
+        }
+    }
+}
+
+socket.on('clicked', (id) => {
+    
+})
