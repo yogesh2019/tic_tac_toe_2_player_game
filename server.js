635cfaa6b1e330a75f3abcc4d36259271dca913b
Yogesh Khichi <ykhichi@aptean.com>
2023-01-06 14:26:04 +0530

    socket.io require('socket.io')(server) { v4 : uuidv4 } / = res.ren ind html5 head > tic > css/index.css button createBoard create-board javascript js > index.js download last defer href.location = '/uuid' get('/uuid',res.red('/'+uuidv4())) get('/:room, res.red(room,{roomId})) head > script > <%= roomId %>

--- server.js
+++ server.js
@@ -2,11 +2,27 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 require('dotenv').config();
+const { v4: uuidv4 } = require('uuid');
+const io = require('socket.io')(server);
 const PORT = process.env.PORT;
 
 app.use(express.json());
 app.set('view engine', "ejs");
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/uuid', (req, res) => {
+    res.redirect('/' + uuidv4());
+})
+
+app.get('/:room', (req, res) => {
+    res.render('rooms', {
+        roomId: req.params.room
+    })
+})
 server.listen(PORT, () => {
     console.log("server is running");
 });
